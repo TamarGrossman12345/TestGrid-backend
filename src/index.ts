15ca924b99684091a46bc51c01cce2012eab7d35
Tamar Grossman <tamar.yust@gmail.com>
2026-04-08 19:58:19 +0300

    adding route to create tests

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import prisma from './db.js';
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import prisma from "./db.js";
 
 // טעינת משתני סביבה מקובץ .env
 dotenv.config();
@@ -15,16 +15,16 @@ app.use(cors()); // מאפשר לפרונטנד שלך (שיושב על פורט
 app.use(express.json()); // מאפשר לשרת לקרוא JSON שנשלח ב-Body של בקשות POST
 
 // נתיב בדיקה בסיסי (Route)
-app.get('/', (req: Request, res: Response) => {
-  res.send('TestGrid API is running successfully! 🚀');
+app.get("/", (req: Request, res: Response) => {
+  res.send("TestGrid API is running successfully! 🚀");
 });
 
 // נתיב ליצירת פרויקט חדש
-app.post('/projects', async (req, res) => {
+app.post("/projects", async (req, res) => {
   try {
     const { projectName, description, isPrivate } = req.body;
     const project = await prisma.project.create({
-      data: { projectName, description, isPrivate }
+      data: { projectName, description, isPrivate },
     });
     res.json({ message: "Project created successfully", project });
   } catch (error) {
@@ -33,13 +33,12 @@ app.post('/projects', async (req, res) => {
   }
 });
 
-
-app.get('/projects', async (req, res) => {
+app.get("/projects", async (req, res) => {
   try {
     const projects = await prisma.project.findMany({
       include: {
-        files: true // פריזמה אוטומטית תצרף לכל פרויקט את רשימת הקבצים שלו
-      }
+        files: true, // פריזמה אוטומטית תצרף לכל פרויקט את רשימת הקבצים שלו
+      },
     });
     res.json(projects);
   } catch (error) {
@@ -48,11 +47,11 @@ app.get('/projects', async (req, res) => {
   }
 });
 
-app.delete('/projects/:id/', async (req, res) => {
+app.delete("/projects/:id/", async (req, res) => {
   try {
     const id = req.params.id;
-    const deletedProject = await prisma.project.delete({ 
-      where: { projectId: id } 
+    const deletedProject = await prisma.project.delete({
+      where: { projectId: id },
     });
 
     res.json({ message: "Project deleted successfully", deletedProject });
@@ -62,32 +61,30 @@ app.delete('/projects/:id/', async (req, res) => {
   }
 });
 
-// נתיב ליצירת קובץ חדש בתוך פרויקט
-app.post('/files', async (req, res) => {
+app.post("/files", async (req, res) => {
   try {
-    // אנחנו מצפים לקבל מהפרונטנד שם, תיאור ואת ה-ID של הפרויקט האב
     const { name, description, projectId } = req.body;
 
     const newFile = await prisma.testFile.create({
       data: {
         name,
         description,
-        projectId, // זה ה-Foreign Key שמקשר את הקובץ לפרויקט הנכון!
+        projectId, 
       },
     });
 
-    res.status(201).json({ message: "File created successfully", newFile});
+    res.status(201).json({ message: "File created successfully", newFile });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to create file" });
   }
 });
 
-app.delete('/files/:id/', async (req, res) => {
+app.delete("/files/:id/", async (req, res) => {
   try {
     const id = req.params.id;
-    const deletedFile = await prisma.testFile.delete({ 
-      where: { TestFileId: id } 
+    const deletedFile = await prisma.testFile.delete({
+      where: { TestFileId: id },
     });
 
     res.json({ message: "File deleted successfully", deletedFile });
@@ -97,9 +94,47 @@ app.delete('/files/:id/', async (req, res) => {
   }
 });
 
+app.get("/testCase/:fileId", async (req, res) => {
+  try {
+    const { fileId } = req.params;
+
+    const testCases = await prisma.testCase.findMany({
+      where: { fileId: fileId },
+    });
+
+    res.json(testCases);
+  } catch (error) {
+    console.error("Error fetching test cases:", error);
+    res.status(500).json({ error: "Could not fetch test cases" });
+  }
+});
+
+
+app.post('/testCases/newTestCase/:fileId', async (req, res) => {
+  const { fileId } = req.params;
+  const { title, testSteps, expectedResults, status } = req.body;
+
+  try {
+    const newTestCase = await prisma.testCase.create({
+      data: {
+        title,
+        testSteps,
+        expectedResults,
+        status,
+        fileId 
+      },
+    });
+    res.status(201).json(newTestCase);
+  } catch (error: any) {
+    res.status(500).json({ error: "failed to create test case", details: error.message });
+  }
+});
+
 
 app.listen(PORT, () => {
   console.log(`-----------------------------------------`);
   console.log(`Server is running on: http://localhost:${PORT}`);
   console.log(`-----------------------------------------`);
 });
+
+
